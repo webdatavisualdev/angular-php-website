592f73cf3e506d48ad97732ef92f82ee5bdb16fe
webdatavisualdev <chen.li39@zoho.eu>
2017-09-14 23:03:50 +0800

    Update config.php

--- lib/php/config.php
+++ lib/php/config.php
@@ -1,13 +1,9 @@
 <?php
-    // $servername = "172.18.242.121"; //your server name
-    // $username = "myuser"; // your username
-    // $password = "mypass"; //your password
-    // $database = "db_alert"; //your database name, leave it as it is if you have already run "setup.php"
+    $servername = "172.18.242.121"; //your server name
+    $username = "myuser"; // your username
+    $password = "mypass"; //your password
+    $database = "db_alert"; //your database name, leave it as it is if you have already run "setup.php"
 
-      $servername = "172.18.242.121"; //your server name
-      $username = "myuser"; // your username
-      $password = "mypass"; //your password
-      $database = "db_alert";
     $con=mysqli_connect($servername, $username, $password, $database);
     //$con1=mysqli_connect($servername, $username, $password, "dblogsources");
     $con2=mysqli_connect($servername, $username, $password);
