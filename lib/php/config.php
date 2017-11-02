08ca2b9e35cbdc0b63791bf95a01ca3bd9b9d134
webdatavisualdev <chen.li39@zoho.eu>
2017-11-02 22:34:18 +0200

    Changed MySQL config

--- lib/php/config.php
+++ lib/php/config.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "172.18.242.121"; //your server name
-    $username = "myuser"; // your username
-    $password = "mypass"; //your password
-    $database = "db_alert"; //your database name, leave it as it is if you have already run "setup.php"
+    $servername = "localhost"; //your server name
+    $username = "root"; // your username
+    $password = "root"; //your password
+    $database = "sac"; //your database name, leave it as it is if you have already run "setup.php"
 
     $con=mysqli_connect($servername, $username, $password, $database);
     //$con1=mysqli_connect($servername, $username, $password, "dblogsources");
